0b27ead359f16e890b074af047172713d216ffe6
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-30 12:40:46 +0300

    Used link

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
       <h1> WorldWise</h1>
+
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
