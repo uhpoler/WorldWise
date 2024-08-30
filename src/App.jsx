a9d61fdeb480d3e4054824a525b43ffd51d06b14
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-30 14:04:02 +0300

    Added nested routes and index route

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
