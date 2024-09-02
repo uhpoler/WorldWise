f0c6249d60b7f8b5b5250c8aa3f30cdf916cd562
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-02 17:55:19 +0300

    Consumed the CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 const BASE_URL = "http://localhost:9000";
 const CitiesContext = createContext();
 
@@ -34,4 +34,10 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) throw new Error("undefined citiescontext");
+  return context;
+}
+
+export { CitiesProvider, useCities };
