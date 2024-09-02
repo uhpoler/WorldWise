561c0e8fb80d4a27886f32cb56980258f4b33cd9
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-02 18:27:57 +0300

    Finished the City View

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,71 +1,82 @@
-// import styles from "./City.module.css";
+import styles from "./City.module.css";
 // import ButtonBack from "./ButtonBack";
 
-import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
-// const formatDate = (date) =>
-//   new Intl.DateTimeFormat("en", {
-//     day: "numeric",
-//     month: "long",
-//     year: "numeric",
-//     weekday: "long",
-//   }).format(new Date(date));
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
+
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
 
 function City() {
   const { id } = useParams();
+  const { getCity, currentCity, isLoading } = useCities();
 
-  const [searchParams, setSerchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  console.log(setSerchParams, lat, lng);
-
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
 
   const { cityName, emoji, date, notes } = currentCity;
-  console.log(cityName, emoji, date, notes);
-  return <h1>City {id}</h1>;
-  //   return (
-  //     <div className={styles.city}>
-  //       <div className={styles.row}>
-  //         <h6>City name</h6>
-  //         <h3>
-  //           <span>{emoji}</span> {cityName}
-  //         </h3>
-  //       </div>
 
-  //       <div className={styles.row}>
-  //         <h6>You went to {cityName} on</h6>
-  //         <p>{formatDate(date || null)}</p>
-  //       </div>
+  if (isLoading) return <Spinner />;
 
-  //       {notes && (
-  //         <div className={styles.row}>
-  //           <h6>Your notes</h6>
-  //           <p>{notes}</p>
-  //         </div>
-  //       )}
+  return (
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{flagemojiToPNG(emoji)}</span> {cityName}
+        </h3>
+      </div>
 
-  //       <div className={styles.row}>
-  //         <h6>Learn more</h6>
-  //         <a
-  //           href={`https://en.wikipedia.org/wiki/${cityName}`}
-  //           target="_blank"
-  //           rel="noreferrer"
-  //         >
-  //           Check out {cityName} on Wikipedia &rarr;
-  //         </a>
-  //       </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
 
-  //       <div></div>
-  //     </div>
-  //   );
-  //
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        {" "}
+        <BackButton />
+      </div>
+    </div>
+  );
 }
 
 export default City;
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -5,6 +5,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -22,11 +23,28 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      console.log(cities);
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
