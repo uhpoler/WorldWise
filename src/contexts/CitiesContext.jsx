1cc108cfc461344dceaf5d5a321408dd97cb6826
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-10-04 09:13:12 +0300

    Updated server rendering

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "/cities.json";
 
 const CitiesContext = createContext();
 
@@ -32,22 +32,6 @@ function reducer(state, action) {
     case "city/loaded":
       return { ...state, isLoading: false, currentCity: action.payload };
 
-    case "city/created":
-      return {
-        ...state,
-        isLoading: false,
-        cities: [...state.cities, action.payload],
-        currentCity: action.payload,
-      };
-
-    case "city/deleted":
-      return {
-        ...state,
-        isLoading: false,
-        cities: state.cities.filter((city) => city.id !== action.payload),
-        currentCity: {},
-      };
-
     case "rejected":
       return {
         ...state,
@@ -71,9 +55,10 @@ function CitiesProvider({ children }) {
       dispatch({ type: "loading" });
 
       try {
-        const res = await fetch(`${BASE_URL}/cities`);
+        const res = await fetch(BASE_URL);
         const data = await res.json();
-        dispatch({ type: "cities/loaded", payload: data });
+        // Зчитування масиву міст із об'єкта
+        dispatch({ type: "cities/loaded", payload: data.cities });
       } catch {
         dispatch({
           type: "rejected",
@@ -86,62 +71,18 @@ function CitiesProvider({ children }) {
 
   const getCity = useCallback(
     async function getCity(id) {
-      if (Number(id) === currentCity.id) return;
-
-      dispatch({ type: "loading" });
-
-      try {
-        const res = await fetch(`${BASE_URL}/cities/${id}`);
-        const data = await res.json();
-        dispatch({ type: "city/loaded", payload: data });
-      } catch {
+      const city = cities.find((city) => city.id === Number(id));
+      if (!city) {
         dispatch({
           type: "rejected",
-          payload: "There was an error loading the city...",
+          payload: "City not found...",
         });
+        return;
       }
+      dispatch({ type: "city/loaded", payload: city });
     },
-    [currentCity.id]
+    [cities]
   );
-
-  async function createCity(newCity) {
-    dispatch({ type: "loading" });
-
-    try {
-      const res = await fetch(`${BASE_URL}/cities`, {
-        method: "POST",
-        body: JSON.stringify(newCity),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-
-      dispatch({ type: "city/created", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error creating the city...",
-      });
-    }
-  }
-
-  async function deleteCity(id) {
-    dispatch({ type: "loading" });
-
-    try {
-      await fetch(`${BASE_URL}/cities/${id}`, {
-        method: "DELETE",
-      });
-
-      dispatch({ type: "city/deleted", payload: id });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error deleting the city...",
-      });
-    }
-  }
 
   return (
     <CitiesContext.Provider
@@ -151,8 +92,6 @@ function CitiesProvider({ children }) {
         currentCity,
         error,
         getCity,
-        createCity,
-        deleteCity,
       }}
     >
       {children}
